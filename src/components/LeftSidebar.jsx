0a4dd5424d161a484a6109328f1d9576ca90d9a4
HenishNaliyadhara2508 <henish@hexacoder.com>
2025-03-07 18:01:11 +0530

    LeftSide bar added

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function LeftSidebar() {
   return (
-    <div className='w-[360px] d-flex border h-[calc(100vh-97px)]'></div>
+    <div className='w-[360px] d-flex border h-[calc(100vh-97px)]'>
+      <nav className="flex justify-between gap-3">
+              <a href="#">My Portfolios</a>
+              <FaRegFolder className="text-2xl hover:text-blue-500"/>
+            </nav>
+            <div className="border-t border-gray-300 my-4"></div>
+      
+            {/* Search Box */}
+            <div className="border border-gray-300 rounded-md flex items-center px-3 py-2">
+              {/* <Search className="text-gray-500" size={18} /> */}
+              <input
+                type="text"
+                placeholder="Search..."
+                className="ml-2 w-full outline-none bg-transparent"
+              />
+            </div>
+      
+            <div className="border-t border-gray-300 my-4"></div>
+            <div className="w-[300px] h-[300px] ">
+            <Card imageSrc="/logo.png" title="Logo" description="The logo 1" />
+            </div>
+            <div className="w-[300px] h-[300px] ">
+            <Card imageSrc="/logo.png" title="Logo" description="The logo 1" />
+            </div>
+    </div>
 
   )
 }
