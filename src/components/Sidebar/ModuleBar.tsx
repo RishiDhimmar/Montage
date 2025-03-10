ab03b2d9fa701322def8146b28d82d577cb8c40f
HenishNaliyadhara2508 <henish@hexacoder.com>
2025-03-10 16:45:11 +0530

    minor change in module bar logic

--- src/components/Sidebar/ModuleBar.tsx
+++ src/components/Sidebar/ModuleBar.tsx
@@ -52,12 +52,16 @@ const ModuleBar: React.FC = observer(() => {
               <img
                 src={module.image}
                 alt={module.name}
-                className="w-full h-[200px] object-cover rounded"
+                className=" h-[200px] object-cover rounded "
               />
-              <p className="text-center mt-2 font-medium">{module.name}</p>
-              <p className="text-center text-gray-600">Type: {module.moduleType}</p>
-              <p className="text-center text-gray-600">Size: {module.size} sqft</p>
-              <p className="text-center text-gray-600 font-semibold">${module.price}</p>
+              <div className="text-sm mt-2 px-2 font-medium">{module.name}</div>
+              <div className="flex justify-between text-sm px-2">
+              <div className="text-center text-gray-600">${module.price}</div>
+              <div className="text-center text-gray-600">{module.noOfBathRooms} Bathroom</div>
+              <div className="text-center text-gray-600">{module.noOfBedRooms} Bedroom</div>
+              <div className="text-center text-gray-600">{module.size} sqft</div>
+              </div>
+              
             </div>
           ))}
         </div>
